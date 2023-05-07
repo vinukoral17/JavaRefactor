1698372021f63c80a1579ff1ef1103c0e6beec01
vinuk <vinuk@VinujaKoral>
2023-05-08 00:32:55 +0100

    Data Clump - Refactored

--- src/base/Domino.java
+++ src/base/Domino.java
@@ -8,96 +8,86 @@ package base;
  * @author Kevan Buckley, maintained by __student
  * @version 2.0, 2014
  */
-
 public class Domino implements Comparable<Domino> {
-	public int high;
-	public int low;
-	public int hx;
-	public int hy;
-	public int lx;
-	public int ly;
-	public boolean placed = false;
+    private final DominoValues values;
+    private int hx;
+    private int hy;
+    private int lx;
+    private int ly;
+    private boolean placed = false;
 
-	public Domino(int high, int low) {
-		super();
-		this.high = high;
-		this.low = low;
-	}
+    public Domino(int high, int low) {
+        this.values = new DominoValues(high, low);
+    }
 
-	public void place(int highX, int highY, int lowX, int lowY) {
-		this.hx = highX;
-		this.hy = highY;
-		this.lx = lowX;
-		this.ly = lowY;
-		placed = true;
-	}
+    public void place(int highX, int highY, int lowX, int lowY) {
+        this.hx = highX;
+        this.hy = highY;
+        this.lx = lowX;
+        this.ly = lowY;
+        placed = true;
+    }
 
-	public String toString() {
-		StringBuffer result = new StringBuffer();
-		result.append("[");
-		result.append(Integer.toString(high));
-		result.append(Integer.toString(low));
-		result.append("]");
-		if (!placed) {
-			result.append("unplaced");
-		} else {
-			result.append("(");
-			result.append(Integer.toString(hx + 1));
-			result.append(",");
-			result.append(Integer.toString(hy + 1));
-			result.append(")");
-			result.append("(");
-			result.append(Integer.toString(lx + 1));
-			result.append(",");
-			result.append(Integer.toString(ly + 1));
-			result.append(")");
-		}
-		return result.toString();
-	}
+    public String toString() {
+        StringBuffer result = new StringBuffer();
+        result.append("[");
+        result.append(Integer.toString(values.getHigh()));
+        result.append(Integer.toString(values.getLow()));
+        result.append("]");
+        if (!placed) {
+            result.append("unplaced");
+        } else {
+            result.append("(");
+            result.append(Integer.toString(hx + 1));
+            result.append(",");
+            result.append(Integer.toString(hy + 1));
+            result.append(")");
+            result.append("(");
+            result.append(Integer.toString(lx + 1));
+            result.append(",");
+            result.append(Integer.toString(ly + 1));
+            result.append(")");
+        }
+        return result.toString();
+    }
 
-	/** turn the domino around 180 degrees clockwise */
+    public void invert() {
+        int temp = hx;
+        hx = lx;
+        lx = temp;
 
-	/*
-	 * ##### - UNUSED VARIABLES The tx and ty variables in the invert() method are
-	 * used only for temporary storage and can be eliminated.
-	 */
+        temp = hy;
+        hy = ly;
+        ly = temp;
+    }
 
-	public void invert() {
-		
-		 int temp = hx;
-		    hx = lx;
-		    lx = temp;
+    public boolean isDominoHorizontal() {
+        return hy == ly;
+    }
 
-		    temp = hy;
-		    hy = ly;
-		    ly = temp;
-	}
+    public int compareTo(Domino arg0) {
+        if (values.getHigh() < arg0.values.getHigh()) {
+            return 1;
+        }
+        return values.getLow() - arg0.values.getLow();
+    }
+}
 
-	/*
-	 * Method to check if the domino is horizontal
-	 * 
-	 * @return boolean value indicating if the domino piece is placed horizontally
-	 */
-	public boolean isDominoHorizontal() {
-		return hy == ly;
-	}
 
-	/*
-	 * The method CompareTo() compared 'Domino' objects based on thier high and low
-	 * values
-	 * 
-	 * @param arg() the Domino object to tbe compared with this object.
-	 * 
-	 * @return an integer value indicating the order of the two objects If
-	 * 'this.higher' is less than `arg0.high` it returns `1` Otherwise, it return
-	 * `this.low - arg0.low`.
-	 */
+class DominoValues {
+    private final int high;
+    private final int low;
 
-	public int compareTo(Domino arg0) {
-		if (this.high < arg0.high) {
-			return 1;
-		}
-		return this.low - arg0.low;
-	}
+    public DominoValues(int high, int low) {
+        this.high = high;
+        this.low = low;
+    }
 
+    public int getHigh() {
+        return high;
+    }
+
+    public int getLow() {
+        return low;
+    }
 }
