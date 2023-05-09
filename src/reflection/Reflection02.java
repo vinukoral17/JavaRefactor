566cf0e51f572d5864512876a2b927bf364de095
vinuk <vinuk@VinujaKoral>
2023-05-09 10:00:42 +0100

    Reflection Experiment 02

---
+++ src/reflection/Reflection02.java
@@ -0,0 +1,13 @@
+package reflection;
+
+public class Reflection02 {
+	public static void main(String[] args) {
+		SimpleMultiply s = new SimpleMultiply();
+	    s.multiplyFloatA();
+	    // s.squareB(); // if you uncomment this you will get a compiler error
+	    float a = s.a;
+	    // int b = s.b; // if you uncomment this you will get a compiler error
+	    System.out.println("s=" + s);
+	  }
+
+}
