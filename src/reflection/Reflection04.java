a141f540d663133962207b880a9cb6506539227a
vinuk <vinuk@VinujaKoral>
2023-05-09 10:01:13 +0100

    Reflection Experiment 04

---
+++ src/reflection/Reflection04.java
@@ -0,0 +1,15 @@
+package reflection;
+import java.lang.reflect.Field;
+
+public class Reflection04 {
+	  public static void main(String[] args) throws Exception {
+	    SimpleMultiply s = new SimpleMultiply();
+	    Field[] fields = s.getClass().getFields();
+	    System.out.printf("There are %d fields\n", fields.length);
+	    for (Field f : fields) {
+	      System.out.printf("field name=%s type=%s value=%d\n", f.getName(),
+	          f.getType(), f.getInt(s));
+	    }
+	  }
+	}
+
