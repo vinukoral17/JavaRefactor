ed38f7a84e82856a54b0db22624974e4956e212e
vinuk <vinuk@VinujaKoral>
2023-05-09 10:17:45 +0100

    Reflection Experiment 08

---
+++ src/reflection/Reflection08.java
@@ -0,0 +1,21 @@
+package reflection;
+
+import java.lang.reflect.*;
+
+
+
+public class Reflection08 {
+  public static void main(String[] args) throws Exception {
+	SimpleMultiply s = new SimpleMultiply();
+    Field[] fields = s.getClass().getDeclaredFields();
+    System.out.printf("There are %d fields\n", fields.length);
+    for (Field f : fields) {
+      f.setAccessible(true);
+      int x = f.getInt(s);
+      x++;
+      f.setInt(s, x);
+      System.out.printf("field name=%s type=%s value=%d\n", f.getName(),
+          f.getType(), f.getInt(s));
+    }
+  }
+}
