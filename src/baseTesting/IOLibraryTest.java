614c0f114e8b4c83a55e198d15fb7f9f0dd9fad4
vinuk <vinuk@VinujaKoral>
2023-05-09 08:48:24 +0100

    Testing - IOLibrary

--- src/baseTesting/IOLibraryTest.java
+++ src/baseTesting/IOLibraryTest.java
@@ -1,14 +1,36 @@
 package baseTesting;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 import org.junit.jupiter.api.Test;
 
-class IOLibraryTest {
+import base.IOLibrary;
 
-	@Test
-	void test() {
-		fail("Not yet implemented");
-	}
+import org.junit.jupiter.api.Assertions;
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.net.InetAddress;
 
+public class IOLibraryTest {
+
+    @Test
+    public void testGetString() {
+        String input = "Hello, world!";
+        InputStream in = new ByteArrayInputStream(input.getBytes());
+        System.setIn(in);
+
+        String output = IOLibrary.getString();
+
+        Assertions.assertEquals(input, output);
+    }
+
+    @Test
+    public void testGetIPAddress() {
+        String input = "127.0.0.1";
+        InputStream in = new ByteArrayInputStream(input.getBytes());
+        System.setIn(in);
+
+        InetAddress output = IOLibrary.getIPAddress();
+
+        Assertions.assertNotNull(output);
+        Assertions.assertEquals(input, output.getHostAddress());
+    }
 }
