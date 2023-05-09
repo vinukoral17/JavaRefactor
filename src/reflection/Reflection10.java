4c10df21703a14e61fcb6278410c908cb69269e5
vinuk <vinuk@VinujaKoral>
2023-05-09 10:18:11 +0100

    Reflection Experiment 10

---
+++ src/reflection/Reflection10.java
@@ -0,0 +1,13 @@
+package reflection;
+
+import java.lang.reflect.Method;
+
+public class Reflection10 {
+	  public static void main(String[] args) throws Exception {
+		SimpleMultiply s = new SimpleMultiply();
+	    Method m = s.getClass().getDeclaredMethod("setA", int.class);
+	    m.setAccessible(true);
+	    m.invoke(s, 76);
+	    System.out.println(s);
+	  }
+	}
