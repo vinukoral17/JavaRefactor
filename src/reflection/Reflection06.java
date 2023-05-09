c601022710067f6a9c4242bb7a5d5650efa511ed
vinuk <vinuk@VinujaKoral>
2023-05-09 10:17:26 +0100

    Reflection Experiment 06

---
+++ src/reflection/Reflection06.java
@@ -0,0 +1,18 @@
+package reflection;
+
+import java.lang.reflect.Field;
+
+
+
+public class Reflection06 {
+  public static void main(String[] args) throws Exception {
+    SimpleMultiply s = new SimpleMultiply();
+    Field[] fields = s.getClass().getDeclaredFields();
+    System.out.printf("There are %d fields\n", fields.length);
+
+    for (Field f : fields) {
+      System.out.printf("field name=%s type=%s accessible=%s\n", f.getName(),
+          f.getType(), f.isAccessible());
+    }
+  }
+}
